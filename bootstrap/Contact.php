7fdad1cb7f5302d1767bec1120015ed12de00564
aro04 <mandaaroniaina2@gmail.com>
2024-04-03 11:08:56 -0400

    commit final

--- bootstrap/Contact.php
+++ bootstrap/Contact.php
@@ -17,7 +17,7 @@
             <form method="POST" action="ContactTraitement.php">
                 <div class="container mt-3">
                     <div class="mb-3 mt-3">
-                        <label for="Username">Username*</label>
+                        <label for="Username">Last Name*</label>
                         <input type="text" class="form-control" id="Username" name="Username" placeholder="Username" required>
                     </div>
                     <div class="mb-3">
@@ -34,7 +34,33 @@
         </div>
     </div>
     </div>
+    <script>
+        function validateForm() {
+            var username = document.getElementById('Username').value;
+            var email = document.getElementById('Email').value;
+            var message = document.getElementById('Message').value;
+            // Validation du prénom et du message
+            var nameRegex = /^[a-zA-Z\-]+$/;
+            if ((nameRegex.test(username) === false)) {
+                alert("Le prénom ne doit contenir que des lettres et des tirets (-).");
+                return false;
+            }
+            var messageRegex = /^[a-zA-Z\-.,'!? \p{L}']+$/u;
+            if ((messageRegex.test(username) === false)) {
+                alert("Le  message ne doit pas contenir des caractères spéciaux comme @,%,{} .");
+                return false;
+            }
 
+            // Validation de l'email
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(email)) {
+                alert("Veuillez saisir une adresse e-mail valide.");
+                return false;
+            }
+
+            return true;
+        }
+    </script>
 </body>
 
 </html>
--- bootstrap/ContactTraitement.php
+++ bootstrap/ContactTraitement.php
@@ -10,12 +10,28 @@ try {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Vérifier que les champs requis ne sont pas vides
-    if (!empty($_POST['Username']) && !empty($_POST['Email_Adress']) && !empty($_POST['Message'])) {
-        $Username = $_POST['Username'];
-        $Email_Adress = $_POST['Email_Adress'];
-        $Message = $_POST['Message'];
+    $errors = array();
+    $Username = $_POST['Username'];
+    $Email_Adress = $_POST['Email_Adress'];
+    $Message = $_POST['Message'];
+    if (!preg_match('/^[a-zA-Z\-]+$/', $Username)) {
+        $errors[] = "Le prénom ne doit contenir que des lettres et des tirets (-).";
+    }
 
+    if (!preg_match("/^[a-zA-Z\-.,'!? \p{L}']+$/u", $Message)) {
+        $errors[] = "Le message ne doit pas contenir des caractères spéciaux comme @ et {}. Les caractères autorisés sont les lettres, les tirets, les points, les virgules, les apostrophes, les points d'interrogation, les points d'exclamation et les espaces.";
+    }
+
+
+    // Validation de l'email
+    if (!filter_var($Email_Adress, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Veuillez saisir une adresse e-mail valide.";
+    }
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo "<p>$error</p>";
+        }
+    } else {
         $request = $bdd->prepare("INSERT INTO contact (Username, Email_Adress, Message) VALUES (:Username, :Email_Adress, :Message)");
         $request->execute(
             array(
@@ -25,8 +41,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             )
         );
 
+
         echo "Données insérées avec succès.";
-    } else {
-        echo "Tous les champs sont requis.";
+        echo '<a href="Shop.php" class="btn btn-primary">Continue</a>';
     }
 }
